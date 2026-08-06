06fa20e5b1b9ad8a1460bef6f2ece5dbed5a59ab
Liudvig <71966781+Liudvig@users.noreply.github.com>
2026-08-06 14:22:07 +0300

    Load restored Posts and Pages routes first

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ require __DIR__.'/app/bootstrap.php';
 $router = new \Kovcheg\Router();
 require __DIR__.'/routes/blog-preflight.php';
 require __DIR__.'/routes/blog-branding.php';
+require __DIR__.'/routes/blog-content-model.php';
 require __DIR__.'/routes/blog-wordpress-compat.php';
 require __DIR__.'/routes/blog-wordpress-mode.php';
 require __DIR__.'/routes/blog-simple-mode.php';
