0db14edbab505a9971dab78d520d5c4b0ab33d42
Liudvig <71966781+Liudvig@users.noreply.github.com>
2026-08-06 14:58:55 +0300

    Load flexible menu styles in Studio

--- views/studio/layout.php
+++ views/studio/layout.php
@@ -36,6 +36,7 @@ $copyright='© '.date('Y').' Ланцет Семён Борисович';
 <link rel="stylesheet" href="<?=e(app_url('/assets/css/blog-studio-wordpress.css?v='.rawurlencode(ASSET_REVISION)))?>">
 <link rel="stylesheet" href="<?=e(app_url('/assets/css/blog-studio-pages.css?v='.rawurlencode(ASSET_REVISION)))?>">
 <link rel="stylesheet" href="<?=e(app_url('/assets/css/blog-studio-branding.css?v='.rawurlencode(ASSET_REVISION)))?>">
+<link rel="stylesheet" href="<?=e(app_url('/assets/css/blog-studio-menus.css?v='.rawurlencode(ASSET_REVISION)))?>">
 </head>
 <body class="studio-body studio-body--simple studio-body--wordpress studio-body--pages" data-studio-section="<?=e($studioSection)?>">
 <div class="studio-shell">
